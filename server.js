df6a68edb22ebc05e972136b0f7cf08692aad6f4
mill <ph.mill@gmail.com>
2017-12-15 17:46:03 +0100

    port correction

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
-const port = process.env.PORT || 4200;
+const port = process.env.port || 4200;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
